ed8db842a9407d98728bd6c4555b4ed703615747
GProgrammerN <guilherme.programador12@gmail.com>
2023-06-20 13:36:34 -0300

    URL ajeitada.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function selecionar(pokemonID){
     var getPokemonID = pokemonID
     localStorage.setItem("pokemonID", getPokemonID)
-    window.location.href = "http://localhost:5500/batalha.html"
+    var URLatual = window.location.hostname
+    window.location.href = `${URLatual}/batalha.html`
 }
